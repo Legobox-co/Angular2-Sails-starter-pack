538dbfc48cdb8b9b5412a10e7d9f43ce131d3dc3
Chibuike Emmauel Osita <mozart.osita@gmail.com>
2017-04-22 23:08:53 +0100

    Major Components realigned and designed

---
+++ assets/app/Components/HomeCompoment/home.component.ts
@@ -0,0 +1,6 @@
+import {Component} from '@angular/core';
+@Component({
+    selector: 'home-view',
+    template: './home.component.html'
+})
+export class HomeComponent { }
---
+++ assets/app/Components/NavComponent/nav.component.ts
@@ -0,0 +1,6 @@
+import {Component} from '@angular/core';
+@Component({
+    selector: 'nav-app',
+    template: './nav.component.html'
+})
+export class NavComponent { }

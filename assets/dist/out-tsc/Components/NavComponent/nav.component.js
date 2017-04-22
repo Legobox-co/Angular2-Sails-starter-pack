2d7960d3b42151358db36e42c848b4330bd4d262
Chibuike Emmauel Osita <mozart.osita@gmail.com>
2017-04-22 23:06:41 +0100

    New dist mappings

---
+++ assets/dist/out-tsc/Components/NavComponent/nav.component.js
@@ -0,0 +1,20 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+import { Component } from '@angular/core';
+var NavComponent = (function () {
+    function NavComponent() {
+    }
+    return NavComponent;
+}());
+NavComponent = __decorate([
+    Component({
+        selector: 'nav-app',
+        template: './nav.component.html'
+    })
+], NavComponent);
+export { NavComponent };
+//# sourceMappingURL=/home/mozartted/PROJECTS/javascript/angular2SailsStarterPack/Components/NavComponent/nav.component.js.map

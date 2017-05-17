929f65a33140524d0cf140653b07c0c56dfe85ec
Chibuike Emmauel Osita <mozart.osita@gmail.com>
2017-05-17 22:21:13 +0100

    Using the new templateUrl pattern

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: './app.component.html'
+    templateUrl: './app.component.html'
 })
 export class AppComponent { }

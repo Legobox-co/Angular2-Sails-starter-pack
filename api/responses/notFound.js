3bdbac15ca7650cc48056f9b9acba66222c71861
Ramzi Jiryes <ramzijiryes@gmail.com>
2018-02-06 12:54:36 +0200

    Bumped Sails to V1.0.0-46 and migrated with necessary changes

--- api/responses/notFound.js
+++ api/responses/notFound.js
@@ -43,7 +43,7 @@ module.exports = function notFound (data, options) {
   // If the user-agent wants JSON, always respond with JSON
   // If views are disabled, revert to json
   if (req.wantsJSON || sails.config.hooks.views === false) {
-    return res.jsonx(data);
+    return res.json(data);
   }
 
   // If second argument is a string, we take that to mean it refers to a view.
@@ -84,7 +84,7 @@ module.exports = function notFound (data, options) {
       else {
         sails.log.warn('res.notFound() :: When attempting to render error page view, an error occured (sending JSON instead).  Details: ', err);
       }
-      return res.jsonx(data);
+      return res.json(data);
     }
 
     return res.send(html);
